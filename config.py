60d1b3f4e5e975f4f5bc74b03b123a6eb739a9b8
Zh-Jiaqi <zhang_jiaqi@hrbeu.edu.cn>
2026-06-23 01:20:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -12,11 +12,11 @@ configs.scale = 2
 
 configs.device = torch.device('cuda:0')
 
-configs.batch_size = 32   #8#4倍4； 8倍32； #16
+configs.batch_size = 32
 configs.batch_size_test = 1
 
 configs.epochs = 100
-configs.lr = 0.001 #原 0.001； swin 0.0002
+configs.lr = 0.001
 configs.opt_size = 128
 
 configs.dims = ['u100', 'v100']
@@ -25,7 +25,7 @@ configs.train_shuffle = False
 
 configs.num_layers = 6
 configs.num_heads = 6
-configs.display_interval = 30  # 75 #4倍200， 8倍80
+configs.display_interval = 30 
 configs.warm_up = 1000
 configs.patience = 100
 
